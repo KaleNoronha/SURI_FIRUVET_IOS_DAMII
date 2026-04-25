dc054a247da2d83769e7acfd4d50acb182767514
XCODE <damii@IMACC202-ST09.local>
2026-04-25 14:54:26 -0500

    fix:se actualizo vistas

--- Suri Firuvet/controller/MenuPrincipalController.swift
+++ Suri Firuvet/controller/MenuPrincipalController.swift
@@ -1,29 +1,73 @@
-//
-//  MenuPrincipalController.swift
-//  Suri Firuvet
-//
-//  Created by XCODE on 18/04/26.
-//
-
 import UIKit
 
 class MenuPrincipalController: UIViewController {
-
+    @IBOutlet weak var imgCrearCita: UIImageView!
+    @IBOutlet weak var imgIngresarMascota: UIImageView!
+    @IBOutlet weak var imgListaDeCitas: UIImageView!
+    
+    @IBOutlet weak var imgListaDeMascotas: UIImageView!
+    @IBOutlet weak var imgPerfilPersonal: UIImageView!
+    
+    @IBOutlet weak var imgVolver: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupTapGestures()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupTapGestures() {
+        let imagenes = [ imgCrearCita, imgIngresarMascota, imgListaDeCitas, imgListaDeMascotas, imgPerfilPersonal, imgVolver ]
+        
+        for imagen in imagenes {
+            imagen?.isUserInteractionEnabled = true
+            
+            let tapGesture = UITapGestureRecognizer(target: self, action: #selector(imagenTapped(_:)))
+            imagen?.addGestureRecognizer(tapGesture)
+        }
     }
-    */
-
+    
+    @objc func imagenTapped(_ sender: UITapGestureRecognizer) {
+        guard let viewTapped = sender.view else { return }
+        
+        switch viewTapped {
+        case imgVolver:
+            if let nav = navigationController {
+                nav.popViewController(animated: true)
+            } else {
+                dismiss(animated: true)
+            }
+            
+        case imgCrearCita:
+            navegarA(identificador: "CrearCitaController")
+            
+        case imgListaDeMascotas:
+            navegarA(identificador: "ListaMascotasController")
+            
+        case imgIngresarMascota:
+            navegarA(identificador: "IngresarMascotaController")
+            
+        case imgListaDeCitas:
+            navegarA(identificador: "ListaCitasController")
+            
+        case imgPerfilPersonal:
+            navegarA(identificador: "PerfilPersonalController")
+            
+        default:
+            break
+        }
+    }
+    
+    private func navegarA(identificador: String) {
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: identificador) else {
+            print("Error: No se encontro el ID \(identificador)")
+            return
+        }
+        
+        if let nav = navigationController {
+            nav.pushViewController(vc, animated: true)
+        } else {
+            present(vc, animated: true)
+        }
+    }
+    
 }
--- Suri Firuvet/controller/PerfilMascotaController.swift
+++ Suri Firuvet/controller/PerfilMascotaController.swift
@@ -1,29 +1,60 @@
-//
-//  PerfilMascotaController.swift
-//  Suri Firuvet
-//
-//  Created by XCODE on 18/04/26.
-//
-
 import UIKit
 
 class PerfilMascotaController: UIViewController {
-
+    @IBOutlet weak var imgListaDeMascotas: UIImageView!
+    @IBOutlet weak var imgPerfilPersonal: UIImageView!
+    
+    @IBOutlet weak var imgVolver: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        setupTapGestures()
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private func setupTapGestures() {
+        let imagenes = [ imgListaDeMascotas, imgPerfilPersonal, imgVolver ]
+        
+        for imagen in imagenes {
+            imagen?.isUserInteractionEnabled = true
+            
+            let tapGesture = UITapGestureRecognizer(target: self, action: #selector(imagenTapped(_:)))
+            imagen?.addGestureRecognizer(tapGesture)
+        }
     }
-    */
-
+    
+    @objc func imagenTapped(_ sender: UITapGestureRecognizer) {
+        guard let viewTapped = sender.view else { return }
+        
+        switch viewTapped {
+        case imgVolver:
+            if let nav = navigationController {
+                nav.popViewController(animated: true)
+            } else {
+                dismiss(animated: true)
+            }
+            
+        case imgListaDeMascotas:
+            navegarA(identificador: "ListaMascotasController")
+            
+        case imgPerfilPersonal:
+            navegarA(identificador: "PerfilPersonalController")
+            
+        default:
+            break
+        }
+    }
+    
+    private func navegarA(identificador: String) {
+        guard let vc = storyboard?.instantiateViewController(withIdentifier: identificador) else {
+            print("Error: No se encontro el ID \(identificador)")
+            return
+        }
+        
+        if let nav = navigationController {
+            nav.pushViewController(vc, animated: true)
+        } else {
+            present(vc, animated: true)
+        }
+    }
+    
 }
